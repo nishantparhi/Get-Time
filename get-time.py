c6486fd4958f22f4c7775732fe7435be450a9945
Nishant Parhi <nishantparhi@gmail.com>
2018-11-29 22:48:02 +0530

    Update get-time.py

--- get-time.py
+++ get-time.py
@@ -4,11 +4,12 @@ import json
 
 os.system("curl 'http://worldtimeapi.org/api/ip' -s > mytime")
 
-with open("mytime", "r+") as time:
-    time = json.loads(time.read())
 
 datatoread = ["utc_offset","abbreviation","timezone","datetime"]
 
 for key in time:
         if key in datatoread:
             print("{}: {}".format(key, time[key]))
+           
+with open("mytime", "r+") as time:
+    time = json.loads(time.read())
